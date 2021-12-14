47b52b22c43940f63beac304c2aebd131d376a8b
Jenny Schutzman <jenny.schutzman@ratequote.com>
2021-12-14 08:51:17 -0500

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom'
+
+import Home from './pages/home/home.jsx'
+// import AboutMe from './pages/aboutMe/aboutMe.jsx'
+// import FormerLife from './pages/formerLife/formerLife.jsx'
+// import MyCode from './pages/myCode/code.jsx'
+// import TechCommunity from './pages/techCommunity/techCommunity.jsx'
+
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/aboutMeNow" component={AboutMe} />
+        <Route exact path="/myFormerLife" component={FormerLife} />
+        <Route exact path="/myCode" component={MyCode} />
+        <Route exact path="/involvementInTechCommunity" component={TechCommunity} /> */}
+      </Switch>
+    </BrowserRouter>
+  )
 }
+
 
 export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from 
+
+const Home = () => {
+
+    return (
+        <Header 
+        title="Jenny Schutzman, Front-end Software Engineer"
+        subheader="languages here"
+        />
+    )
+}
