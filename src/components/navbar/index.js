d674dbfd19af7f5ade8183004a0ed89b8b07a57c
Jenny Schutzman <jenny.schutzman@ratequote.com>
2021-12-20 10:35:04 -0500

    cleaned up cardData in tech-com

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
--- src/pages/tech-community/tech-com-data.js
+++ src/pages/tech-community/tech-com-data.js
@@ -6,7 +6,7 @@ export const cardData = [
         img: "/images/edWIT.png",
         category: "Executive Director",
         title: "Charleston Women In Tech",
-        description: "in charge of overseeing the strategic vision of the organiza)on and leading business decisions to drive growth in membership and impact to the community. Duties include collaborating with the Board of Directors, implemen)ng strategic plans for the organiza)on’s development and managing relatonships with stakeholders.",
+        description: "Nn charge of overseeing the strategic vision of the organiza)on and leading business decisions to drive growth in membership and impact to the community. Duties include collaborating with the Board of Directors, implementing strategic plans for the organization’s development and managing relatonships with stakeholders.",
         linkTitle: "Learn more about CHSWIT",
         link: `https://chswomenintech.org/`
     },
@@ -15,7 +15,7 @@ export const cardData = [
         img: "/images/CHSWITpink.png",
         category: "Program Coordinator",
         title: "Charleston Women in Tech Mentorship Program",
-        description: "In 2019, created a mentorship program consistanting of 100+ women in Charleston area working in technology.  This consisted of recruiting, program creation, pairing, organization and faciliation of formal workshop to kick-off program",
+        description: "In 2019, created a mentorship program consistanting of 100+ women in Charleston area working in technology.  This included of recruiting, program creation, pairing, organization and faciliation of formal workshop to kick-off program",
         linkTitle: "Learn more about CHSWIT",
         link: `https://chswomenintech.org/`
     },
@@ -24,7 +24,7 @@ export const cardData = [
         img: "/images/womenDevs.jpeg",
         category: "Founder",
         title: "Charleston Women Devs",
-        description: "Charleston Women Devs went strong for 3 years, before being abosbred into the larger Women in Tech.  We did monthly meetups; both social happy hours to network and technical presenation for skills-building. It's been an unique and wonderful experience to create a casual group of female software engineers that would rarely get to talk code with friends in our normal out-of-work settings.",
+        description: "Charleston Women Devs went strong for 3 years, before being absorbed into the larger Women in Tech in December of 2021.  We did monthly meetups; both social happy hours to network and technical presenation for skills-building. It's been an unique and wonderful experience to create a casual group of female software engineers that would rarely get to talk code with friends in our normal out-of-work settings.",
         linkTitle: "Group now in CHSWIT",
         link: `https://chswomenintech.org/`
     },
